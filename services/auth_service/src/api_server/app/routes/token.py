91dab5f2103b65996a2f94f803c03f791ed66ba3
4ntonch3 <anton.chelpanov.1978@mail.ru>
2024-03-17 13:19:40 +0300

    Add changes to auth API

--- services/auth_service/src/api_server/app/routes/token.py
+++ services/auth_service/src/api_server/app/routes/token.py
@@ -9,20 +9,20 @@ token_router = APIRouter(prefix="/api/v1/token", tags=["TOKEN"])
 @token_router.post(
     path="",
     status_code=status.HTTP_200_OK,
-    responses={status.HTTP_200_OK: {"model": schema.response.create_token.CreateTokenResponse}},
+    responses={status.HTTP_200_OK: {"model": schema.response.CreateTokenResponse}},
 )
 async def create_token(body: schema.request.CreateTokenRequestBody):
     token = await dependency.create_token.execute(body.username, body.secret)
 
-    return schema.response.create_token.CreateTokenResponse.from_token(token)
+    return schema.response.CreateTokenResponse.from_token(token)
 
 
-@token_router.post(
+@token_router.get(
     path="/worker",
     status_code=status.HTTP_200_OK,
-    responses={status.HTTP_200_OK: {"model": schema.response.get_user_by_token.GetWorkerByTokenResponse}},
+    responses={status.HTTP_200_OK: {"model": schema.response.GetWorkerByTokenResponse}},
 )
 async def get_worker_by_token(body: schema.request.GetWorkerByTokenRequestBody):
     worker = await dependency.get_worker_by_token.execute(body.token)
 
-    return schema.response.get_user_by_token.GetWorkerByTokenResponse.from_domain(worker)
+    return schema.response.GetWorkerByTokenResponse.from_domain(worker)
--- services/auth_service/src/api_server/app/routes/worker.py
+++ services/auth_service/src/api_server/app/routes/worker.py
@@ -3,15 +3,15 @@ from fastapi import APIRouter, status
 from .. import dependency, schema
 
 
-worker_router = APIRouter(prefix="/api/v1/worker", tags=["WORKER"])
+workers_router = APIRouter(prefix="/api/v1/workers", tags=["WORKERS"])
 
 
-@worker_router.post(
+@workers_router.post(
     path="",
     status_code=status.HTTP_200_OK,
     responses={status.HTTP_200_OK: {"model": schema.response.EmptyResponse}},
 )
-async def create_worker(body: schema.request.CreateWorkerRequestBody):
-    await dependency.create_worker.execute(body.username, body.secret, body.email, body.role)
+async def register_worker(body: schema.request.RegisterWorkerRequestBody):
+    await dependency.register_worker.execute(body.username, body.secret, body.email, body.role)
 
     return schema.response.EmptyResponse()
